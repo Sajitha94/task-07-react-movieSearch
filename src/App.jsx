b19e31c2ff30910ad5230e967959e8199797b07e
Sajitha94 <sajithasubramani94@gmail.com>
2025-08-17 02:00:08 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/header'
+import Header from "./components/header";
 import MovieCard from './components/MovieCard'
 
 function App() {
