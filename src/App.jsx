0bf44d9d2a697cfd8757d94ed412894c3a78485d
Sajitha94 <sajithasubramani94@gmail.com>
2025-08-17 01:56:12 +0530

    movie card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/header'
+import MovieCard from './components/MovieCard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
     <Header></Header>
-      <h1>saji</h1>
+     <MovieCard></MovieCard>
     </>
   )
 }
