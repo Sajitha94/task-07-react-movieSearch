bd8a1c7d8f6a0d6ab9d6d0cc72bcfe5c6e9f76b7
Sajitha94 <sajithasubramani94@gmail.com>
2025-08-18 01:10:15 +0530

    push

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,22 +1,9 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { ExpandMore } from '@mui/icons-material';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import CircleOutlinedIcon from '@mui/icons-material/CircleOutlined';
 import { useMovieData } from './MovieFetchData';
 import { Box } from '@mui/material';
 import defaultImage from '../assets/noImage.png'
